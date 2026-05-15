239f68b43e6e8f6632def1891aa4a387e6516381
Lorenzo Bizzotto <lorenzo.bizzotto@impresoftengage.com>
2026-05-15 14:41:50 +0200

    fix: Add custom Expo plugin to inject Google Maps API key in AndroidManifest

---
+++ WhatDoWeDO/plugins/withGoogleMapsApiKey.js
@@ -0,0 +1,34 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+module.exports = function withGoogleMapsApiKey(config) {
+  return withAndroidManifest(config, async (config) => {
+    const apiKey = config.android?.googleMapsApiKey;
+    
+    if (!apiKey) {
+      console.warn('No googleMapsApiKey found in app.json');
+      return config;
+    }
+
+    const androidManifest = config.modResults;
+    const mainApplication = androidManifest.manifest.application[0];
+
+    // Remove any existing Google Maps API key meta-data
+    if (mainApplication['meta-data']) {
+      mainApplication['meta-data'] = mainApplication['meta-data'].filter(
+        (item) => item.$['android:name'] !== 'com.google.android.geo.API_KEY'
+      );
+    } else {
+      mainApplication['meta-data'] = [];
+    }
+
+    // Add the new Google Maps API key meta-data
+    mainApplication['meta-data'].push({
+      $: {
+        'android:name': 'com.google.android.geo.API_KEY',
+        'android:value': apiKey,
+      },
+    });
+
+    return config;
+  });
+};
